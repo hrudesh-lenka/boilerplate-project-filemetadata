da9baf309077cac4c56b149bae99193bb6c4f144
hrudesh-lenka <hrudesh.lenka@gmail.com>
2021-07-28 22:27:40 +0530

    added multer

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+let multer = require('multer');
 var app = express();
 
 app.use(cors());
@@ -11,7 +11,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res)=>{
+  let fileData = {};
+  fileData['name'] = req.file.originalname;
+  fileData['type'] = req.file.mimetype;
+  fileData['size'] = req.file.size;
+  res.json(fileData);
+})
 
 
 const port = process.env.PORT || 3000;
